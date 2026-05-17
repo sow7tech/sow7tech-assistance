558a4404b4316ac6d7fcd2738937f50284614d7b
sow7tech <sow7tech@gmail.com>
2026-05-17 01:52:27 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,4 @@
 'use client';
-
-export default function Home() {
-  return <div style={{ padding: '20px', textAlign: 'center', color: '#fff', background: '#020817', minHeight: '100vh' }}>
-    <h1>🎉 Sow7Tech Assistance</h1>
-    <p>L'app est en ligne !</p>
-  </div>;
-}
+import dynamic from 'next/dynamic';
+const App = dynamic(() => import('./app'), { ssr: false });
+export default App;
