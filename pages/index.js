2a3b6f4ef885fde0d6b4910966bf042f782a870a
sow7tech <sow7tech@gmail.com>
2026-05-17 01:40:57 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+'use client';
+
+export default function Home() {
+  return <div style={{ padding: '20px', textAlign: 'center', color: '#fff', background: '#020817', minHeight: '100vh' }}>
+    <h1>🎉 Sow7Tech Assistance</h1>
+    <p>L'app est en ligne !</p>
+  </div>;
+}
