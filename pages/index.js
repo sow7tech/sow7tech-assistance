e228822e5520b78c8e36b22985125b9af16dd77e
sow7tech <sow7tech@gmail.com>
2026-05-17 01:57:19 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 'use client';
 import dynamic from 'next/dynamic';
-const App = dynamic(() => import('./app'), { ssr: false });
+const App = dynamic(() => import('./app.jsx'), { ssr: false });
 export default App;
