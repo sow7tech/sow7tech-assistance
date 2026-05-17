e2841c2fe61444ae2f8e27a425a867ec45b68c65
sow7tech <sow7tech@gmail.com>
2026-05-17 01:23:15 +0100

    Delete pages/app.jsx

--- pages/app.jsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-export default function App() {
-  return <div style={{ padding: '20px', textAlign: 'center', color: '#fff', background: '#020817', minHeight: '100vh' }}>
-    <h1>🎉 Sow7Tech Assistance est EN LIGNE !</h1>
-    <p>L'app fonctionne. Les modules seront ajoutés progressivement.</p>
-  </div>;
-}
